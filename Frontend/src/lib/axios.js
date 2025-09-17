cd4399564e36c0a8dd25e9aab257f507af54307b
Rudra-alpha <rp9970713998@gmail.com>
2025-09-17 10:03:25 +0530

    fixing

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "/api",
+      : "https://hulla-chat-app-7-backend.onrender.com/api", // <-- backend render URL
   withCredentials: true,
 });
